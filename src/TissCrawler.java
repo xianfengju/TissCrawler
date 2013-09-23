40e2b53cbb23ee05ad1c75f49e5570d2656efdd9
Firespy <alexander.svozil@gmail.com>
2013-09-23 17:59:15 +0200

    maven

--- src/TissCrawler.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Alexander
- * Date: 23.09.13
- * Time: 17:57
- * To change this template use File | Settings | File Templates.
- */
-public class TissCrawler {
-}
